06ac5ba808bd8929b0c68b7469ab3add1a7b5a6f
joelsinnott <joelsinnott@hotmail.com>
2023-01-18 07:56:44 +0100

    nodejs, npm, sass

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
+function toggleOverlay() {
+    var overlay = document.getElementById("overlay");
+    overlay.classList.toggle("active");
+}
+
 function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("active");
+    toggleOverlay();
 }
 
+/* close menu with esc key */
 document.addEventListener("keyup", function(event) {
-    if (event.keyCode === 27) {
-        var menu = document.getElementById("menu");
-        if (menu.classList.contains("active")) {
-            toggleMenu();
-        }
+    var menu = document.getElementById("menu");
+    if (event.keyCode === 27 && menu.classList.contains("active")) {
+    toggleMenu();
     }
-});
-
-window.addEventListener("DOMContentLoaded", function() {
-    document.addEventListener("click", function(e) {
-        var menu = document.getElementById("menu");
-        var menuIcon = document.querySelector(".menu-icon");
-        if (!menu.contains(e.target) && !menuIcon.contains(e.target) && menu.classList.contains("active")) {
-            toggleMenu();
-        }
     });
+
+document.getElementById("overlay").addEventListener("click", function() {
+    toggleMenu();
 });
